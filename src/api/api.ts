d1a7907a691b3cfc3e272fc78e53b62309d9e3fa
Gustavo Maia <ms.gustavo@outlook.com>
2024-04-17 11:23:16 -0300

    feat: logica para requisicao em api

---
+++ src/api/api.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000/data";
+
+export const fetchData = async (page: number = 1) => {
+  try {
+    const response = await axios.get(`${BASE_URL}?_page=${page}`);
+    return response.data;
+  } catch (error) {
+    console.error("Erro em requisição de dados:", error);
+    return [];
+  }
+};
+
+export const fetchMedicineById = async (id: string) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error em requisição de remédios:", error);
+    return null;
+  }
+};
